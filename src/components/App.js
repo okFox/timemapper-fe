73b382f3902d32ccb0884dab90e6f02f97b60689
okFox <smayfield@gmail.com>
2020-04-19 15:25:37 -0700

    state getting from App to Block but not to Activity

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,64 @@
-import React from 'react';
+
 // import Activities from './Activities';
+import React, { useState } from 'react';
 import Block from './Block';
 import ActivityForm from './ActivityForm';
 import './App.css';
 
+const blockData = {
+
+  userId: 'user1',
+  blockName: 'Learn Japanese',
+  timeUnitInMin: 15
+
+};
+
+
+const someActivities = {
+  activities: [
+    {
+      activityID: 100,
+      activityName: 'Wani Kani',
+      duration: 3,
+      color: 'rgba(255, 0, 0, 0.2)',
+      description: 'Complete Wani Kani levels.',
+      position: {
+        x: '', // or start end using the timeUnits
+        y: ''
+      } },
+    {
+      activityID: 200,
+      activityName: 'Glossika',
+      duration: 2,
+      color: 'rgba(0, 255, 0, 0.3)',
+      description: 'Listen to Glossika sentences',
+      position: {
+        x: '', // or start end using the timeUnits
+        y: ''
+      } },
+    {
+      activityID: 300,
+      activityName: 'Kanji',
+      duration: 1,
+      color: 'rgba(0, 0, 255, 0.3)',
+      description: 'Practice writing basic 100',
+      position: {
+        x: '', // or start end using the timeUnits
+        y: ''
+      } }
+  ]
+};
+
+// const blockContext = React.createContext({ blockData });
+
 export default function App() {
+  const [blockState, setBlockState] = useState({blockData});
+  const [activitiesState, setActivitiesState] = useState([someActivities]);
+  
   return (
     <>
       <ActivityForm />
-      <Block />
+      <Block blockState={blockState} activitiesState={activitiesState}/>
     </>
   );
 }
--- src/components/Block.js
+++ src/components/Block.js
@@ -1,59 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Activity from './Activity';
 import styles from './Block.css';
 
 
-function Block() {
-  const [blockState, setBlockState] = useState({
+function Block(props) {
+  const block = props.blockState;
 
-    userId: 'user1',
-    blockName: 'Learn Japanese',
-    timeUnitInMin: 15
-
-  });
-  const [activitiesState, setActivitiesState] = useState({
-    activities: [
-      {
-        activityID:'id1',
-        activityName: 'Wani Kani',
-        duration: 3,
-        color: 'rgba(255, 0, 0, 0.2)',
-        description: 'Complete Wani Kani levels.',
-        position: {
-          x: '', // or start end using the timeUnits
-          y: ''
-        } },
-      {
-        activityID:'id2',
-        activityName: 'Glossika',
-        duration: 2,
-        color: 'rgba(0, 255, 0, 0.3)',
-        description: 'Listen to Glossika sentences',
-        position: {
-          x: '', // or start end using the timeUnits
-          y: ''
-        } },
-      {
-        activityID:'id3',
-        activityName: 'Kanji',
-        duration: 1,
-        color: 'rgba(0, 0, 255, 0.3)',
-        description: 'Practice writing basic 100',
-        position: {
-          x: '', // or start end using the timeUnits
-          y: ''
-        } }
-    ]
-  });
+  const currentActivities = props.activitiesState;
+  // console.log(currentActivities + ' block');
   
-
 
   return (
     <div className={styles.blockContainer}>
-      <h1>{blockState.blockName}</h1>
+      <h1>{block.blockName}</h1>
+
       <div className={styles.block}>
-        {activitiesState.activities.map((activity) => <Activity key={activity.activityName} name={activity.activityName} description={activity.description} duration={activity.duration * blockState.timeUnitInMin} color={activity.color} height={activity.duration * 15 }/>)}
+        {currentActivities.map((activity) => 
+          <Activity activity={props.activity} key={activity.activityId}  name={activity.activityName} description={activity.description} duration={activity.duration * block.timeUnitInMin} color={activity.color} height={activity.duration * 15 }/>)}
       </div>
+
     </div>
   );
 }
