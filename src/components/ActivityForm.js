cbfc042c86becfc1d5adfa7a10aac194443a62ff
okFox <smayfield@gmail.com>
2020-04-19 08:49:07 -0700

    save point before I make big changes

--- src/components/ActivityForm.js
+++ src/components/ActivityForm.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Activity.css';
 
 
 const activityForm = () => {
 
+  // const handleInputChange = (event) => {
+  //   let target = event.target;
+
+  // };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log('event listener works!');
+    console.log(formData);
   };
 
   return (
     <div className={styles.form}>
       <form onSubmit={handleSubmit}>
         <h1>Add a New Activity</h1>
-        <label htmlFor='actname'> Activity Name:</label>
-        <input type='text' name='actname' id='actname' placeholder='eg. Learn Piano'></input>
-        <label htmlFor='actdesc'>Description:</label>
-        <input type='textarea' name='actdesc' id='actdesc'></input>
+
+        <label> Activity Name:</label>
+        <input type='text' name='activityName' id='actname' placeholder='eg. Learn Piano'></input>
+
+        <label>Description:</label>
+        <input type='textarea' name='description' id='actdesc'></input>
+
         <label htmlFor="dur">Duration in minutes:</label>
-        <input type='number' id="dur" name="dur" min="0" max='120' step='15'></input>
+        <input type='number' id="dur" name='duration' min="0" max='120' step='15'></input>
+
         <button id='submit' value='submit'>Create Activity</button>
       </form>
     </div>
