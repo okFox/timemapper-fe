bf882629feab2fe096dd6305b44dbe465f2b1242
okFox <smayfield@gmail.com>
2020-04-18 20:25:47 -0700

    form submit wired up

--- src/components/ActivityForm.js
+++ src/components/ActivityForm.js
@@ -4,9 +4,14 @@ import styles from './Activity.css';
 
 const activityForm = () => {
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('event listener works!');
+  };
+
   return (
     <div className={styles.form}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Add a New Activity</h1>
         <label htmlFor='actname'> Activity Name:</label>
         <input type='text' name='actname' id='actname' placeholder='eg. Learn Piano'></input>
@@ -14,7 +19,7 @@ const activityForm = () => {
         <input type='textarea' name='actdesc' id='actdesc'></input>
         <label htmlFor="dur">Duration in minutes:</label>
         <input type='number' id="dur" name="dur" min="0" max='120' step='15'></input>
-        <button>Create Activity</button>
+        <button id='submit' value='submit'>Create Activity</button>
       </form>
     </div>
   );
--- src/components/Block.js
+++ src/components/Block.js
@@ -52,7 +52,7 @@ function Block() {
     <div className={styles.blockContainer}>
       <h1>{blockState.blockName}</h1>
       <div className={styles.block}>
-        {activitiesState.activities.map((activity) => <Activity key={activity.activityId} name={activity.activityName} description={activity.description} duration={activity.duration * blockState.timeUnitInMin} color={activity.color} height={activity.duration * 15 }/>)}
+        {activitiesState.activities.map((activity) => <Activity key={activity.activityName} name={activity.activityName} description={activity.description} duration={activity.duration * blockState.timeUnitInMin} color={activity.color} height={activity.duration * 15 }/>)}
       </div>
     </div>
   );
